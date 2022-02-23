abfb52316119a5a76daa5924aadf79ee072e285f
sbelambe <46766748+sbelambe@users.noreply.github.com>
2022-02-22 20:22:54 -0800

    Add files via upload

---
+++ shwordle.java
@@ -0,0 +1,54 @@
+package shwordle;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class shwordle {
+
+	public static String words[] = { "water", "walks", "candy", "troll", "barks" };	
+	public static int rand = (int) (Math.random() * 4 + 1);
+	public static boolean go = true;
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = 1;
+		while(go && num<7) {
+			System.out.print("Enter your word: ");
+			String inputWord = scan.nextLine();
+			System.out.println(match(inputWord));
+			num++;
+		}
+	}
+	
+
+	public static StringBuffer match(String input) {
+
+		StringBuffer sb = new StringBuffer();
+		
+		ArrayList<Character> al = new ArrayList<Character>();
+		for(int k=0; k<5; k++) {
+			al.add(words[rand].charAt(k));
+		}
+		
+		for (int i=0; i<5; i++) {
+			if(al.contains(input.charAt(i))) {
+				if (input.charAt(i) == words[rand].charAt(i)) {
+					 sb.append("y");
+				}
+				else {
+					sb.append("*");
+				}
+			}
+			else {
+				sb.append("n");
+			}
+		}
+		
+		if("yyyyy".contentEquals(sb)) {
+			go = false;
+		}
+		
+		return sb;
+	}
+
+}
